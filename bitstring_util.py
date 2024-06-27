9a776d5a846599aed50b4fe5ff980739cf1764b6
bdg221 <brian.goldsmith@gmail.com>
2024-06-27 09:33:18 -0700

    handle leading zeroes

--- bitstring_util.py
+++ bitstring_util.py
@@ -1,13 +1,22 @@
 # This module is used for generating a random bitstring of a specified size.
+#
 # Code originally from https://www.geeksforgeeks.org/python-program-to-generate-random-binary-string/
-# and contributed by Edula Vinay Kumar Reddy
+# and contributed by Edula Vinay Kumar Reddy.
+#
+# Modified to handle leading zeroes.
 
 import random
 
-def generate_binary_string(n):
-	# Generate a random number with n bits
-	number = random.getrandbits(n)
-	# Convert the number to binary
-	binary_string = format(number, '0b')
-	return binary_string
 
+def generate_binary_string(n):
+    # Generate a random number with n bits
+    number = random.getrandbits(n)
+
+    # Convert the number to binary
+    binary_string = format(number, "0b")
+
+    # handle leading zeroes
+    if len(binary_string) != n:
+        binary_string = ("0" * (n - len(binary_string))) + binary_string
+
+    return binary_string
